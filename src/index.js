d299ce25f2b9436d6b7409693aa485c26e96c435
Malnati <ricardomalnati@gmail.com>
2023-11-04 00:34:19 +0100

    chore: Update watch:src command and fix typo in React component - Updated the `watch:src` command in `package.json` to include a `git push` after running the `commit-msg` script. - Fixed a typo in the React component `NavigationBar`, changing "React!" to "React !".

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 
 function NavigationBar() {
   // TODO: Actually implement a navigation bar
-  return <h1>Hello from React!</h1>;
+  return <h1>Hello from React in!</h1>;
 }
 
 const domNode = document.getElementById('navigation');
