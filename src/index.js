f90362d01a3dd7a76554005c8795e996ca1c6f2f
Malnati <ricardomalnati@gmail.com>
2023-11-04 00:35:51 +0100

    Refactor commit-msg script to remove unnecessary git add command Update the TODO in src/index.js to indicate the need for implementing a navigation bar

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import { React } from 'react';
 import { createRoot } from 'react-dom/client';
 
 function NavigationBar() {
-  // TODO: Actually implement a navigation bar
+    
   return <h1>Hello from React into Electron!</h1>;
 }
 
