5fe7bd85ed7f28644201aa3d97e1152e464ca351
Malnati <ricardomalnati@gmail.com>
2023-11-04 03:08:59 +0100

    Added a Footer component with two image logos and styling.

--- src/index.js
+++ src/index.js
@@ -12,6 +12,29 @@ const setBgImage = () => {
     document.body.style.backgroundAttachment = 'fixed';
 }
 
+const Footer = () => {
+    return (
+        <footer style={footerStyle}>
+            <img src={logo1} alt="Logo 1" style={logoStyle} />
+            <img src={logo2} alt="Logo 2" style={logoStyle} />
+        </footer>
+    );
+}
+
+const footerStyle = {
+    position: 'fixed',
+    bottom: 0,
+    width: '100%',
+    display: 'flex',
+    justifyContent: 'space-between',
+    padding: '10px',
+    backgroundColor: 'rgba(255, 255, 255, 0.8)',  // fundo branco semi-transparente
+}
+
+const logoStyle = {
+    width: '100px',  // ou qualquer outro tamanho que desejar
+}
+
 const container = document.createElement('div');
 document.body.appendChild(container);
 const root = createRoot(container);
@@ -19,6 +42,10 @@ const root = createRoot(container);
 setBgImage();
 
 root.render(
-    <h1>
-        <center>Bem vindo</center>
-    </h1>);
+    <>
+        <h1>
+            <center>Bem vindo</center>
+        </h1>
+        <Footer />
+    </>
+);
