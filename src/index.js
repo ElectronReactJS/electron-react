f867915aac28241bb95dc3fca9f8e49ef5f90e38
Malnati <ricardomalnati@gmail.com>
2023-11-04 03:12:28 +0100

    Update background color of footer to be slightly more opaque.

--- src/index.js
+++ src/index.js
@@ -27,7 +27,7 @@ const footerStyle = {
     display: 'flex',
     justifyContent: 'space-between',
     padding: '10px',
-    backgroundColor: 'rgba(255, 255, 255, 0.8)',  // fundo branco semi-transparente
+    backgroundColor: 'rgba(255, 255, 255, 0.9)',  // fundo branco semi-transparente
 }
 
 const logoStyle = {
