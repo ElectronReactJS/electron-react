acc38bc35a62a6ba91da17cdfed97c7638bf159c
Malnati <ricardomalnati@gmail.com>
2023-11-04 02:40:50 +0100

    Add "setBgImage()" function call to set the background image in the renderer.js file.

--- src/renderer.js
+++ src/renderer.js
@@ -12,4 +12,6 @@ const container = document.createElement('div');
 document.body.appendChild(container);
 const root = createRoot(container);
 
+setBgImage();
+
 root.render(<h1>This message is being logged by "renderer.js", included via webpack</h1>);
