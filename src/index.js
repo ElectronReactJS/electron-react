2a6ac313d4cfb790ef14d6c8931362da176f48af
Malnati <ricardomalnati@gmail.com>
2023-11-04 04:27:06 +0100

    Changed the file `src/index.js` to update the import paths for the logo images. Replaced `logoMDS.png` with `logoMDS.svg`, and replaced `logoFADEX.jpg` with `logoFADEX.svg`.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import backgroundImage from './assets/background.jpg';
-import logoLeft from './assets/logoMDS.png';
-import logoRight from './assets/logoFADEX.jpg';
+import logoLeft from './assets/logoMDS.svg';
+import logoRight from './assets/logoFADEX.svg';
 import EmailField from './component/EmailField';
 
 
