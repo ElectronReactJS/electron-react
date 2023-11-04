a23a802633991ca3032aad7f619307f846544daf
Malnati <ricardomalnati@gmail.com>
2023-11-04 03:44:08 +0100

    Added new component `EmailField.jsx` which is a functional component. Imported images `success.svg` and `error.svg`. Added state variables `email` to store the email input and `validationImage` to display the validation status. Implemented `validateEmail` function to validate the email using a regular expression. Implemented `handleBlur` function to set the validation image based on the email input. Rendered an input field to capture the email input and display the validation image if present.

---
+++ src/component/EmailField.jsx
@@ -0,0 +1,52 @@
+import React, { useState } from 'react';
+import successImage from './assets/success.svg';
+import errorImage from './assets/error.svg';
+
+const EmailField = () => {
+    const [email, setEmail] = useState('');
+    const [validationImage, setValidationImage] = useState(null);
+
+    const validateEmail = (email) => {
+        const regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+        return regex.test(email);
+    };
+
+    const handleBlur = () => {
+        setValidationImage(validateEmail(email) ? successImage : errorImage);
+    };
+
+    return (
+        <div style={emailFieldStyle}>
+            <input
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                onBlur={handleBlur}
+                onKeyPress={(e) => e.key === 'Enter' && handleBlur()}
+                style={inputStyle}
+            />
+            {validationImage && <img src={validationImage} alt="Validation status" style={imageStyle} />}
+        </div>
+    );
+};
+
+const emailFieldStyle = {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    margin: 'auto',
+    width: '300px',
+    padding: '10px',
+};
+
+const inputStyle = {
+    width: '80%',
+    padding: '5px',
+};
+
+const imageStyle = {
+    width: '20px',
+    marginLeft: '10px',
+};
+
+export default EmailField;
