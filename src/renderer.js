c5a5e36820b52e7eeb854bedec5cc3e40590f718
Malnati <ricardomalnati@gmail.com>
2023-11-04 02:05:55 +0100

    [Feature] Update package-lock.json and renderer.js - Update chokidar-cli dependency version to 3.0.0 in package-lock.json - Update npm-run-all dependency version to 4.1.5 in package-lock.json - Add a new function `fetchImages()` in renderer.js to fetch images from the Unsplash API and set the background of an element with the response data

--- src/renderer.js
+++ src/renderer.js
@@ -28,6 +28,26 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 
+const fechIamages = () => {
+    fetch("https://api.unsplash.com/search/photos?query=philippines&client_id=2BHRf_91BeuRTt7CDCE-_eA3l95wlZLWlyog-KQ2c2Y")
+        .then(
+            function (response) {
+                if (response.status !== 200) {
+                    console.log("There was a problem. Status code: " + response.status);
+                    return;
+                }
+                response.json().then(
+                    function (data) {
+                        document.getElementById("image").style.background = 'url(' + data["results"][0]["urls"]["regular"] + ')';
+                    }
+                )}
+        )
+        .catch(
+            function (err) {
+                console.log(err + '404');
+            })
+}
+
 const container = document.createElement('div');
 document.body.appendChild(container);
 const root = createRoot(container);
