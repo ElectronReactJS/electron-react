f00cd6811993022765dc83301528bd1563454184
Malnati <ricardomalnati@gmail.com>
2023-11-04 02:10:15 +0100

    Refactor fetchImages function and update background style The commit message reflects the changes made in the git diff output: - Renamed the function `fechIamages` to `fetchImages` - Updated the background style from `document.getElementById("image").style.background` to `document.body.style.background` - Added a comment indicating the purpose of the `fetchImages` function and calling it at the end of the file.

--- src/renderer.js
+++ src/renderer.js
@@ -1,34 +1,7 @@
-/**
- * This file will automatically be loaded by webpack and run in the "renderer" context.
- * To learn more about the differences between the "main" and the "renderer" context in
- * Electron, visit:
- *
- * https://electronjs.org/docs/tutorial/application-architecture#main-and-renderer-processes
- *
- * By default, Node.js integration in this file is disabled. When enabling Node.js integration
- * in a renderer process, please be aware of potential security implications. You can read
- * more about security risks here:
- *
- * https://electronjs.org/docs/tutorial/security
- *
- * To enable Node.js integration in this file, open up `main.js` and enable the `nodeIntegration`
- * flag:
- *
- * ```
- *  // Create the browser window.
- *  mainWindow = new BrowserWindow({
- *    width: 800,
- *    height: 600,
- *    webPreferences: {
- *      nodeIntegration: true
- *    }
- *  });
- * ```
- */
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 
-const fechIamages = () => {
+const fetchImages = () => {
     fetch("https://api.unsplash.com/search/photos?query=philippines&client_id=2BHRf_91BeuRTt7CDCE-_eA3l95wlZLWlyog-KQ2c2Y")
         .then(
             function (response) {
@@ -38,7 +11,7 @@ const fechIamages = () => {
                 }
                 response.json().then(
                     function (data) {
-                        document.getElementById("image").style.background = 'url(' + data["results"][0]["urls"]["regular"] + ')';
+                        document.body.style.background = 'url(' + data["results"][0]["urls"]["regular"] + ')';
                     }
                 )}
         )
@@ -53,3 +26,6 @@ document.body.appendChild(container);
 const root = createRoot(container);
 
 root.render(<h1>This message is being logged by "renderer.js", included via webpack</h1>);
+
+// Call the fetchImages function to fetch the image and set the background
+fetchImages();
