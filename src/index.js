01d2ff05a2696670d77e9e16005d0846c377b77b
Malnati <ricardomalnati@gmail.com>
2023-11-04 04:34:57 +0100

    Refactor the index.js file by removing the unused imports for logoLeft and logoRight from the './assets' directory. Add the Footer component to the index.js file and update the setBgImage function by removing the redundant code.

--- src/index.js
+++ src/index.js
@@ -1,39 +1,14 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import backgroundImage from './assets/background.jpg';
-import logoLeft from './assets/linux.svg';
-import logoRight from './assets/react.svg';
 import EmailField from './component/EmailField';
-
+import Footer from './component/Footer';
 
 const setBgImage = () => {
     const bgImage = `url(${backgroundImage})`;
     document.body.style.backgroundImage = bgImage;
     document.body.style.backgroundSize = 'cover';
     document.body.style.backgroundAttachment = 'fixed';
-}
-
-const Footer = () => {
-    return (
-        <footer style={footerStyle}>
-            <img src={logoLeft} alt="Logo Left" style={logoStyle} />
-            <img src={logoRight} alt="Logo Right" style={logoStyle} />
-        </footer>
-    );
-}
-
-const footerStyle = {
-    position: 'fixed',
-    bottom: 0,
-    width: '97%',
-    display: 'flex',
-    justifyContent: 'space-between',
-    padding: '10px',
-    backgroundColor: 'rgba(255, 255, 255, 0.4)',  
-}
-
-const logoStyle = {
-    width: '100px',  
 }
 
 const container = document.createElement('div');
