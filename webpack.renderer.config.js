0ddd2199bc6bf543aa057217ae3ed2544eed9aa2
Malnati <ricardomalnati@gmail.com>
2023-11-04 02:56:52 +0100

    Refactor code to use `index.js` instead of `renderer.js` This commit updates the `forge.config.js` and `webpack.renderer.config.js` files to use `index.js` instead of `renderer.js`. In the `forge.config.js` file, the entry point for the `main_window` is changed from `js: './src/renderer.js'` to `js: './src/index.js'`. This ensures that the main JavaScript file for the main window is `index.js` instead of `renderer.js`. The `renderer.js` file is also removed in this commit. It is deleted from the repository, as indicated by the `deleted file mode 100644` line in the `git diff` output. In the `webpack.renderer.config.js` file, the filename for the output bundle is changed from `renderer.js` to `index.js`. This ensures that the generated bundle file is named `index.js` instead of `renderer.js`. These changes help to improve the code organization and naming conventions in the project.

--- webpack.renderer.config.js
+++ webpack.renderer.config.js
@@ -10,7 +10,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     publicPath: '/',
-    filename: 'renderer.js',
+    filename: 'index.js',
   },
   module: {
     rules,
