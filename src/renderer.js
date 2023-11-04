1cb2e5c5e9f69cccced86957a0cee9393a98b93b
Malnati <ricardomalnati@gmail.com>
2023-11-04 02:40:23 +0100

    Added a new function `setBgImage()` to set the background image of the document body. The function sets the background image, size, and attachment using inline styles.

--- src/renderer.js
+++ src/renderer.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 
+const setBgImage = () => {
+    const bgImage = 'url(./assets/background.jpg)';  // Ajuste o caminho conforme necessário
+    document.body.style.backgroundImage = bgImage;
+    document.body.style.backgroundSize = 'cover';
+    document.body.style.backgroundAttachment = 'fixed';
+}
+
 const container = document.createElement('div');
 document.body.appendChild(container);
 const root = createRoot(container);
