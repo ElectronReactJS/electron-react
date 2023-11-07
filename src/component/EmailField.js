5e12c63a1161f2d5ac8e42e3c65d27a8b0fef82b
Malnati <ricardomalnati@gmail.com>
2023-11-07 08:11:11 +0100

    MINOR (EmailField 🧩): Refactor EmailField component and update styling
    Refactor EmailField component and update styling
    
    - Added import for atImage from '../assets/at.svg'
    - Changed useState for validationImage to atImage as the initial state
    - Created handleChange function to update the email value and set validationImage based on the input
    - Updated handleBlur function to only set validationImage if email length is greater than 0
    - Modified inputStyle to have width set to 100% to fill the container
    - Added right padding to inputStyle to make room for the validation icon
    - Applied box-sizing: border-box to inputStyle to prevent padding from affecting the total width
    - Modified imageStyle to position the validation icon absolutely within the relative container and align it to the right
    - Removed unused imports
    - Changed position property in emailFieldStyle from 'absolute' to 'relative'
    - Deleted tsconfig.json file

--- src/component/EmailField.js
+++ src/component/EmailField.js
@@ -1,18 +1,31 @@
 import React, { useState } from 'react';
 import successImage from '../assets/success.svg';
 import errorImage from '../assets/error.svg';
+import atImage from '../assets/at.svg'; // Assuming you have an @ image for the initial state
 
 const EmailField = () => {
     const [email, setEmail] = useState('');
-    const [validationImage, setValidationImage] = useState(null);
+    const [validationImage, setValidationImage] = useState(atImage);
 
     const validateEmail = (email) => {
         const regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
         return regex.test(email);
     };
 
+    const handleChange = (e) => {
+        const emailValue = e.target.value;
+        setEmail(emailValue);
+        if (emailValue.length === 0) {
+            setValidationImage(atImage);
+        } else {
+            setValidationImage(validateEmail(emailValue) ? successImage : errorImage);
+        }
+    };
+
     const handleBlur = () => {
-        setValidationImage(validateEmail(email) ? successImage : errorImage);
+        if (email.length > 0) {
+            setValidationImage(validateEmail(email) ? successImage : errorImage);
+        }
     };
 
     return (
@@ -20,12 +33,12 @@ const EmailField = () => {
             <input
                 type="email"
                 value={email}
-                onChange={(e) => setEmail(e.target.value)}
+                onChange={handleChange}
                 onBlur={handleBlur}
                 onKeyPress={(e) => e.key === 'Enter' && handleBlur()}
                 style={inputStyle}
             />
-            {validationImage && <img src={validationImage} alt="Validation status" style={imageStyle} />}
+            <img src={validationImage} alt="Validation status" style={imageStyle} />
         </div>
     );
 };
@@ -37,20 +50,24 @@ const emailFieldStyle = {
     margin: 'auto',
     width: '300px',
     padding: '10px',
-    position: 'absolute',
+    position: 'relative', // Changed from 'absolute' to 'relative'
     top: '50%',
     left: '50%',
     transform: 'translate(-50%, -50%)',
 };
 
 const inputStyle = {
-    width: '80%',
-    padding: '5px',
+    width: '100%', // Changed from '80%' to '100%' to fill the container
+    padding: '5px 30px 5px 5px', // Added right padding to make room for the icon
+    boxSizing: 'border-box', // Ensure padding doesn't affect the total width
 };
 
 const imageStyle = {
+    position: 'absolute', // Position absolutely within the relative container
+    right: '10px', // Distance from the right edge of the container
+    top: '50%', // Center vertically
+    transform: 'translateY(-50%)', // Center vertically
     width: '20px',
-    marginLeft: '10px',
 };
 
 export default EmailField;
