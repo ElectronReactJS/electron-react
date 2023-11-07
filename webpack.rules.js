23533180ef99767b71053be61406229ecf6e03ab
Malnati <ricardomalnati@gmail.com>
2023-11-07 19:26:47 +0100

    INVALID: The commit message must have content to format it correctly. Please provide the necessary information for the commit message.
    null

--- webpack.rules.js
+++ webpack.rules.js
@@ -1,8 +1,5 @@
 module.exports = [
-    // Add support for native node modules
     {
-      // We're specifying native_modules in the test because the asset relocator loader generates a
-      // "fake" .node file which is really a cjs file.
       test: /native_modules[/\\].+\.node$/,
       use: 'node-loader',
     },
@@ -18,11 +15,11 @@ module.exports = [
     },
     {
       test: /\.(js|jsx)$/,
+      exclude: /node_modules/,
       use: {
         loader: 'babel-loader',
         options: {
-          exclude: /node_modules/,
-          presets: ['@babel/preset-react']
+            presets: ['@babel/preset-env', '@babel/preset-react']
         }
       }
     },
@@ -38,20 +35,4 @@ module.exports = [
         },
       ],
     },
-    // Put your webpack loader rules in this array.  This is where you would put
-    // your ts-loader configuration for instance:
-    /**
-     * Typescript Example:
-     *
-     * {
-     *   test: /\.tsx?$/,
-     *   exclude: /(node_modules|.webpack)/,
-     *   loaders: [{
-     *     loader: 'ts-loader',
-     *     options: {
-     *       transpileOnly: true
-     *     }
-     *   }]
-     * }
-     */
   ];
