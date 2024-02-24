3b288ef34aadc45426c01edb7a7127dc6dd2f172
Malnati <ricardomalnati@gmail.com>
2024-02-24 15:06:03 -0300

    path: auto commit by npm run commit

--- src/pages/login/LoginPage.tsx
+++ src/pages/login/LoginPage.tsx
@@ -8,7 +8,7 @@ import TextField from '../../components/wrap/inputs/TextFieldWrapper';
 import Password from '../../components/wrap/inputs/PasswordFieldWrapper';
 import Typography from '../../components/wrap/displays/TypographyWrapper';
 import { LoginPaperTheme } from './LoginPaper.Theme';
-import loginImage from '../images/logo128x128.png';
+// import loginImage from '../images/logo128x128.png';
 
 const LoginPage: React.FC<any> = () => { 
     const navigate = useNavigate();
@@ -24,11 +24,11 @@ const LoginPage: React.FC<any> = () => {
         <Box>
             <Paper sx={loginPaperStyles} elevation={3}>
                 <Box> 
-                    <Typography color="inherit" variant="h5" component="h1">
-                        {label}
-                    </Typography>
                     <Box> 
-                        <img src={loginImage} alt="Logo"/>
+                        <Typography color="inherit" variant="h5" component="h1">
+                            {label}
+                        </Typography>
+                        {/* <img src={loginImage} alt="Logo"/> */}
                     </Box>
                     <TextField label="Username or e-mail" placeholder="endereco@dominio.gov.br" />
                     <Password  onChange={goToMain}/>
