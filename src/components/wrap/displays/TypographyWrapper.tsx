bd649cc4511a76cd951daaa1ef10ad3efad66dd2
Malnati <ricardomalnati@gmail.com>
2024-02-24 13:40:00 -0300

    path: auto commit by npm run commit

--- src/components/wrap/displays/TypographyWrapper.tsx
+++ src/components/wrap/displays/TypographyWrapper.tsx
@@ -1,21 +1,13 @@
 
 // src/components/wrap/displays/TypographyWrapper.tsx
-import Typography, { TypographyProps } from '@mui/material/Typography';
+import Typography from '@mui/material/Typography';
 import { useTheme } from '@mui/material/styles';
-import { TypographyWrapperTheme } from './TypographyWrapper.Theme';
 
-
-
-const TypographyWrapper: React.FC<any> = (props: any) => {
+const TypographyWrapper = (props: any) => {
   const currentTheme = useTheme();
-  const typographyWrapperTheme = TypographyWrapperTheme(currentTheme);
 
   return (
-    <Typography sx={{ ...typographyWrapperTheme, ...props?.sx }} {...props}>
-        <>
-            {props?.children}
-        </>
-    </Typography>
+    <Typography {...props} />
   );
 };
 
