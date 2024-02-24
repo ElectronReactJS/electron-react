aadaddb56a65a2058cd766b116d5bfedd7abc421
Malnati <ricardomalnati@gmail.com>
2024-02-24 14:04:30 -0300

    path: auto commit by npm run commit

--- src/components/wrap/displays/TypographyWrapper.tsx
+++ src/components/wrap/displays/TypographyWrapper.tsx
@@ -2,13 +2,8 @@
 // src/components/wrap/displays/TypographyWrapper.tsx
 import Typography from './TypographyExtended';
 
-const TypographyWrapper: React.FC<any> = (props) => {
-
-  return (
-    <Typography {...props} >
-        {props.children}
-    </Typography>
-  );
-};
+const TypographyWrapper: React.FC<any> = (props) => {  
+    return <Typography {...props} />; 
+  };
 
 export default TypographyWrapper;
