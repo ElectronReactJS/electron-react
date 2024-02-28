5b0f4871cb2faa66c4e754a71315c5f96d974d1a
Malnati <ricardomalnati@gmail.com>
2024-02-28 01:28:17 -0300

    path: commit by npm run format

--- src/pages/documents/DocumentsPageEditor.tsx
+++ src/pages/documents/DocumentsPageEditor.tsx
@@ -1,15 +1,16 @@
 // src/pages/documents/DocumentsPageEditor.tsx
-import React, { useState } from 'react';
-import ReactQuill from 'react-quill';
-import Paper from '../../components/extends/surfaces/PaperWrapper';
-import 'react-quill/dist/quill.snow.css'; 
+import React, {useState} from 'react'
+import ReactQuill from 'react-quill'
+import Paper from '../../components/extends/surfaces/PaperWrapper'
+import 'react-quill/dist/quill.snow.css'
 
 const DocumentsPageEditor: React.FC<any> = () => {
-  const label = 'Documents Editor';
-  const [editorContent, setEditorContent] = useState('');
+  const label = 'Documents Editor'
+  const [editorContent, setEditorContent] = useState('')
 
   return (
-    <Paper elevation={3}
+    <Paper
+      elevation={3}
       sx={{
         display: 'flex',
         flexDirection: 'column', // Garante que os itens dentro do Paper sejam organizados verticalmente
@@ -18,18 +19,19 @@ const DocumentsPageEditor: React.FC<any> = () => {
         height: 'calc(100vh - 100px)', // Ajusta a altura para ocupar quase toda a altura da tela, ajuste conforme necessário
         margin: '50px', // Adiciona margem ao redor do Paper para não tocar as bordas da tela
         padding: '20px' // Adiciona padding dentro do Paper
-      }}>
-      <ReactQuill 
-        theme="snow" 
-        value={editorContent} 
+      }}
+    >
+      <ReactQuill
+        theme='snow'
+        value={editorContent}
         onChange={setEditorContent}
-        style={{ 
+        style={{
           width: '100%', // Faz o ReactQuill ocupar toda a largura disponível
-          height: '85%', // Faz o ReactQuill ocupar a altura disponível
-        }} 
+          height: '85%' // Faz o ReactQuill ocupar a altura disponível
+        }}
       />
     </Paper>
-  );
-};
+  )
+}
 
-export default DocumentsPageEditor;
+export default DocumentsPageEditor
