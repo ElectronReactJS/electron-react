c4f065634aa611f825507441c40089a85cd6521a
Malnati <ricardomalnati@gmail.com>
2024-02-28 17:01:23 -0300

    path: auto commit after npm start

--- src/pages/documents/DocumentsHeader.tsx
+++ src/pages/documents/DocumentsHeader.tsx
@@ -1,13 +1,10 @@
 // src/pages/documents/DocumentsHeader.tsx
 import React, {ReactNode} from 'react'
 import MenuIcon from '@mui/icons-material/Menu'
-import {IconButton, Toolbar} from '@mui/material'
+import {IconButton, Toolbar, Typography} from '@mui/material'
 
 import {styled} from '@mui/material/styles'
 import MuiAppBar, {AppBarProps as MuiAppBarProps} from '@mui/material/AppBar'
-
-import Menu from '../common/menu/Menu'
-import Box from '../../components/extends/layouts/Box'
 
 interface AppBarProps extends MuiAppBarProps {
   open?: boolean
@@ -31,6 +28,28 @@ const StyledAppBar = styled(MuiAppBar, {
   })
 }))
 
+interface AppBarProps extends MuiAppBarProps {
+    open?: boolean;
+  }
+  
+  const AppBar = styled(MuiAppBar, {
+    shouldForwardProp: (prop) => prop !== 'open',
+  })<AppBarProps>(({ theme, open, drawerWidth }) => ({
+    zIndex: theme.zIndex.drawer + 1,
+    transition: theme.transitions.create(['width', 'margin'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    ...(open && {
+      marginLeft: drawerWidth,
+      width: `calc(100% - ${drawerWidth}px)`,
+      transition: theme.transitions.create(['width', 'margin'], {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.enteringScreen,
+      }),
+    }),
+  }));
+
 interface DocumentsHeaderProps {
   handleDrawerOpen: () => void
   open: boolean
@@ -45,28 +64,23 @@ const DocumentsHeader: React.FC<DocumentsHeaderProps> = ({
   children
 }) => {
   return (
-    <StyledAppBar
-      position='fixed'
-      open={open}
-      drawerWidth={drawerWidth}
-      color='primary'
-      enableColorOnDark
-    >
-      <Toolbar color='primary'>
-        <IconButton
-          color='inherit'
-          aria-label='open drawer'
-          onClick={handleDrawerOpen}
-          edge='start'
-          sx={{mr: 2}}
-        >
-          <MenuIcon />
-        </IconButton>
-        {children}
-        <Box flexGrow={1} />
-        <Menu />
-      </Toolbar>
-    </StyledAppBar>
+    <AppBar position="fixed" open={open} drawerWidth={drawerWidth}>
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            onClick={handleDrawerOpen}
+            edge="start"
+            sx={{
+              marginRight: 5,
+              ...(open && { display: 'none' }),
+            }}
+          >
+            <MenuIcon />
+          </IconButton>
+          {children}
+        </Toolbar>
+        </ AppBar>  
   )
 }
 
