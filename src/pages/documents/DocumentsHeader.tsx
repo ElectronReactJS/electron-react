a266c15d629e9a15ee365c15f64ae6ed5d86c477
Malnati <ricardomalnati@gmail.com>
2024-02-28 17:01:24 -0300

    path: commit by npm run format

--- src/pages/documents/DocumentsHeader.tsx
+++ src/pages/documents/DocumentsHeader.tsx
@@ -29,26 +29,26 @@ const StyledAppBar = styled(MuiAppBar, {
 }))
 
 interface AppBarProps extends MuiAppBarProps {
-    open?: boolean;
-  }
-  
-  const AppBar = styled(MuiAppBar, {
-    shouldForwardProp: (prop) => prop !== 'open',
-  })<AppBarProps>(({ theme, open, drawerWidth }) => ({
-    zIndex: theme.zIndex.drawer + 1,
+  open?: boolean
+}
+
+const AppBar = styled(MuiAppBar, {
+  shouldForwardProp: prop => prop !== 'open'
+})<AppBarProps>(({theme, open, drawerWidth}) => ({
+  zIndex: theme.zIndex.drawer + 1,
+  transition: theme.transitions.create(['width', 'margin'], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen
+  }),
+  ...(open && {
+    marginLeft: drawerWidth,
+    width: `calc(100% - ${drawerWidth}px)`,
     transition: theme.transitions.create(['width', 'margin'], {
       easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen,
-    }),
-    ...(open && {
-      marginLeft: drawerWidth,
-      width: `calc(100% - ${drawerWidth}px)`,
-      transition: theme.transitions.create(['width', 'margin'], {
-        easing: theme.transitions.easing.sharp,
-        duration: theme.transitions.duration.enteringScreen,
-      }),
-    }),
-  }));
+      duration: theme.transitions.duration.enteringScreen
+    })
+  })
+}))
 
 interface DocumentsHeaderProps {
   handleDrawerOpen: () => void
@@ -64,23 +64,23 @@ const DocumentsHeader: React.FC<DocumentsHeaderProps> = ({
   children
 }) => {
   return (
-    <AppBar position="fixed" open={open} drawerWidth={drawerWidth}>
-        <Toolbar>
-          <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            onClick={handleDrawerOpen}
-            edge="start"
-            sx={{
-              marginRight: 5,
-              ...(open && { display: 'none' }),
-            }}
-          >
-            <MenuIcon />
-          </IconButton>
-          {children}
-        </Toolbar>
-        </ AppBar>  
+    <AppBar position='fixed' open={open} drawerWidth={drawerWidth}>
+      <Toolbar>
+        <IconButton
+          color='inherit'
+          aria-label='open drawer'
+          onClick={handleDrawerOpen}
+          edge='start'
+          sx={{
+            marginRight: 5,
+            ...(open && {display: 'none'})
+          }}
+        >
+          <MenuIcon />
+        </IconButton>
+        {children}
+      </Toolbar>
+    </AppBar>
   )
 }
 
