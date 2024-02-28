446e36578367e8ce30c7190768d389eb60c0ed35
Malnati <ricardomalnati@gmail.com>
2024-02-28 12:34:16 -0300

    path: auto commit after npm start

--- src/pages/documents/DocumentsDrawer.tsx
+++ src/pages/documents/DocumentsDrawer.tsx
@@ -1,5 +1,6 @@
 // src/pages/documents/DocumentsDrawer.tsximport React from 'react';
-import Drawer from '@mui/material/Drawer'
+// import Drawer from '@mui/material/Drawer'
+import MuiDrawer from '@mui/material/Drawer';
 import List from '@mui/material/List'
 import Divider from '@mui/material/Divider'
 import IconButton from '@mui/material/IconButton'
@@ -9,7 +10,8 @@ import ListItem from '@mui/material/ListItem'
 import ListItemButton from '@mui/material/ListItemButton'
 import ListItemIcon from '@mui/material/ListItemIcon'
 import ListItemText from '@mui/material/ListItemText'
-import {styled, useTheme} from '@mui/material/styles'
+import {Theme, styled, useTheme} from '@mui/material/styles'
+import { CSSObject } from '@emotion/react';
 
 const DrawerHeader = styled('div')(({theme}) => ({
   display: 'flex',
@@ -19,6 +21,44 @@ const DrawerHeader = styled('div')(({theme}) => ({
   justifyContent: 'flex-end'
 }))
 
+  const openedMixin = (theme: Theme, drawerWidth: number): CSSObject => ({
+    width: drawerWidth,
+    transition: theme.transitions.create('width', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+    overflowX: 'hidden',
+  });
+  
+  const closedMixin = (theme: Theme): CSSObject => ({
+    transition: theme.transitions.create('width', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    overflowX: 'hidden',
+    width: `calc(${theme.spacing(7)} + 1px)`,
+    [theme.breakpoints.up('sm')]: {
+      width: `calc(${theme.spacing(8)} + 1px)`,
+    },
+  });
+
+  const Drawer = styled(MuiDrawer, {
+    shouldForwardProp: (prop) => prop !== 'open' && prop !== 'drawerWidth',
+  })(({ theme, open, drawerWidth }: { theme: Theme; open?: boolean; drawerWidth: number; }) => ({
+    width: drawerWidth,
+    flexShrink: 0,
+    whiteSpace: 'nowrap',
+    boxSizing: 'border-box',
+    ...(open && {
+      ...openedMixin(theme, drawerWidth),
+      '& .MuiDrawer-paper': openedMixin(theme, drawerWidth),
+    }),
+    ...(!open && {
+      ...closedMixin(theme),
+      '& .MuiDrawer-paper': closedMixin(theme),
+    }),
+  }));
+
 export interface DrawerItemsType {
   id: string
   icon: JSX.Element
@@ -26,11 +66,11 @@ export interface DrawerItemsType {
 }
 
 interface DocumentsDrawerProps {
-  handleDrawerClose: () => void
-  onPageChange: (pageId: string) => void
-  open: boolean
-  drawerWidth: number
-  items: DrawerItemsType[]
+  handleDrawerClose: () => void;
+  onPageChange: (pageId: string) => void;
+  open: boolean;
+  drawerWidth: number;
+  items: DrawerItemsType[];
 }
 
 const DocumentsDrawer: React.FC<DocumentsDrawerProps> = ({
@@ -42,51 +82,39 @@ const DocumentsDrawer: React.FC<DocumentsDrawerProps> = ({
 }) => {
   const theme = useTheme()
   return (
-    <Drawer
-      sx={{
-        width: drawerWidth,
-        flexShrink: 0,
-        '& .MuiDrawer-paper': {
-          width: drawerWidth,
-          boxSizing: 'border-box'
-        }
-      }}
-      variant='persistent'
-      anchor='left'
-      open={open}
-    >
-      <DrawerHeader>
-        <IconButton onClick={handleDrawerClose}>
-          {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
-        </IconButton>
-      </DrawerHeader>
-      <Divider />
-      <List>
-        {items.map(item => (
-          <ListItem key={item.id} disablePadding sx={{display: 'block'}}>
-            <ListItemButton
-              sx={{
-                minHeight: 48,
-                justifyContent: open ? 'initial' : 'center',
-                px: 2.5
-              }}
-              onClick={() => onPageChange(item.pageId)}
-            >
-              <ListItemIcon
+    <Drawer theme={theme} variant="permanent" open={open} drawerWidth={drawerWidth}>
+    <DrawerHeader>
+      <IconButton onClick={handleDrawerClose}>
+        {theme.direction === 'rtl' ? <ChevronRightIcon /> : <ChevronLeftIcon />}
+      </IconButton>
+    </DrawerHeader>
+    <Divider />
+    <List>
+    {items.map(item => (
+            <ListItem key={item.id} disablePadding sx={{display: 'block'}}>
+                <ListItemButton
                 sx={{
-                  minWidth: 0,
-                  mr: open ? 3 : 'auto',
-                  justifyContent: 'center'
+                    minHeight: 48,
+                    justifyContent: open ? 'initial' : 'center',
+                    px: 2.5
                 }}
-              >
-                {item.icon}
-              </ListItemIcon>
-              <ListItemText primary={item.id} sx={{opacity: open ? 1 : 0}} />
-            </ListItemButton>
-          </ListItem>
+                onClick={() => onPageChange(item.pageId)}
+                >
+                <ListItemIcon
+                    sx={{
+                    minWidth: 0,
+                    mr: open ? 3 : 'auto',
+                    justifyContent: 'center'
+                    }}
+                >
+                    {item.icon}
+                </ListItemIcon>
+                <ListItemText primary={item.id} sx={{opacity: open ? 1 : 0}} />
+                </ListItemButton>
+            </ListItem>
         ))}
-      </List>
-    </Drawer>
+    </List>
+  </Drawer>
   )
 }
 
