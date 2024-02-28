61e0ddc23fa3dca3cb97a064eeb5b98bef6cb9c5
Malnati <ricardomalnati@gmail.com>
2024-02-28 00:50:29 -0300

    path: auto commit after npm start

--- src/pages/documents/DocumentsPageViewer.tsx
+++ src/pages/documents/DocumentsPageViewer.tsx
@@ -1,6 +1,10 @@
 // src/pages/documents/DocumentsPageViewer.tsx
 import Paper from '../../components/extends/surfaces/PaperWrapper'
 import Typography from '../../components/extends/displays/TypographyWrapper'
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import { CardActionArea } from '@mui/material';
 
 const DocumentsPageViewer: React.FC<any> = () => {
   const label = 'Documents Viewer'
@@ -10,8 +14,14 @@ const DocumentsPageViewer: React.FC<any> = () => {
       <Typography color='inherit' variant='h5' component='h1'>
         {label}
       </Typography>
-      <Typography paragraph>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+      <Card sx={{ maxWidth: 700 }}>
+      <CardActionArea>
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            Lizard
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
         labore et dolore magna aliqua. Rhoncus dolor purus non enim praesent elementum facilisis leo
         vel. Risus at ultrices mi tempus imperdiet. Semper risus in hendrerit gravida rutrum quisque
         non tellus. Convallis convallis tellus id interdum velit laoreet id donec ultrices. Odio
@@ -21,18 +31,10 @@ const DocumentsPageViewer: React.FC<any> = () => {
         tincidunt. Cras tincidunt lobortis feugiat vivamus at augue. At augue eget arcu dictum
         varius duis at consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem donec massa
         sapien faucibus et molestie ac.
-      </Typography>
-      <Typography paragraph>
-        Consequat mauris nunc congue nisi vitae suscipit. Fringilla est ullamcorper eget nulla
-        facilisi etiam dignissim diam. Pulvinar elementum integer enim neque volutpat ac tincidunt.
-        Ornare suspendisse sed nisi lacus sed viverra tellus. Purus sit amet volutpat consequat
-        mauris. Elementum eu facilisis sed odio morbi. Euismod lacinia at quis risus sed vulputate
-        odio. Morbi tincidunt ornare massa eget egestas purus viverra accumsan in. In hendrerit
-        gravida rutrum quisque non tellus orci ac. Pellentesque nec nam aliquam sem et tortor.
-        Habitant morbi tristique senectus et. Adipiscing elit duis tristique sollicitudin nibh sit.
-        Ornare aenean euismod elementum nisi quis eleifend. Commodo viverra maecenas accumsan lacus
-        vel facilisis. Nulla posuere sollicitudin aliquam ultrices sagittis orci a.
-      </Typography>
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
     </Paper>
   )
 }
