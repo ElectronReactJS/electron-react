d6c470513c3daa0dcdb66e1ac9ee996a4baa933b
Malnati <ricardomalnati@gmail.com>
2024-02-28 01:28:16 -0300

    path: auto commit after npm start

--- src/pages/documents/DocumentsPageEditor.tsx
+++ src/pages/documents/DocumentsPageEditor.tsx
@@ -1,46 +1,35 @@
 // src/pages/documents/DocumentsPageEditor.tsx
-import React from 'react'
-import Paper from '../../components/extends/surfaces/PaperWrapper'
-import Typography from '../../components/extends/displays/TypographyWrapper'
-import MonacoEditor from 'react-monaco-editor'
+import React, { useState } from 'react';
+import ReactQuill from 'react-quill';
+import Paper from '../../components/extends/surfaces/PaperWrapper';
+import 'react-quill/dist/quill.snow.css'; 
 
 const DocumentsPageEditor: React.FC<any> = () => {
-  const label = 'Documents Editor'
-
-  // Função para manipular mudanças no editor
-  const onChange = (newValue: string, e: any) => {
-    console.log('onChange', newValue, e)
-  }
-
-  // Opções de configuração para o Monaco Editor
-  const editorOptions = {
-    selectOnLineNumbers: true,
-    roundedSelection: false,
-    readOnly: false,
-    cursorStyle: 'line' as const, // Use 'as const' para garantir que o tipo seja reconhecido corretamente
-    automaticLayout: true
-  }
+  const label = 'Documents Editor';
+  const [editorContent, setEditorContent] = useState('');
 
   return (
-    <Paper elevation={3}>
-      <Typography color='inherit' variant='h5' component='h1'>
-        {label}
-      </Typography>
-      <div style={{height: '500px'}}>
-        {' '}
-        {/* Defina o estilo conforme necessário */}
-        <MonacoEditor
-          width='100%'
-          height='400' // Ajuste a altura conforme necessário
-          language='javascript' // Defina a linguagem de programação
-          theme='vs-light' // Tema do editor
-          value={'// Escreva seu código aqui\n'}
-          options={editorOptions}
-          onChange={onChange}
-        />
-      </div>
+    <Paper elevation={3}
+      sx={{
+        display: 'flex',
+        flexDirection: 'column', // Garante que os itens dentro do Paper sejam organizados verticalmente
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: 'calc(100vh - 100px)', // Ajusta a altura para ocupar quase toda a altura da tela, ajuste conforme necessário
+        margin: '50px', // Adiciona margem ao redor do Paper para não tocar as bordas da tela
+        padding: '20px' // Adiciona padding dentro do Paper
+      }}>
+      <ReactQuill 
+        theme="snow" 
+        value={editorContent} 
+        onChange={setEditorContent}
+        style={{ 
+          width: '100%', // Faz o ReactQuill ocupar toda a largura disponível
+          height: '85%', // Faz o ReactQuill ocupar a altura disponível
+        }} 
+      />
     </Paper>
-  )
-}
+  );
+};
 
-export default DocumentsPageEditor
+export default DocumentsPageEditor;
