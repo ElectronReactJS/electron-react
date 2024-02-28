8a88ad0c8d1a6c2e00c0e49774533e8d02487349
Malnati <ricardomalnati@gmail.com>
2024-02-28 01:11:58 -0300

    path: auto commit after npm start

--- src/pages/documents/DocumentsPageViewer.tsx
+++ src/pages/documents/DocumentsPageViewer.tsx
@@ -3,25 +3,32 @@ import Paper from '../../components/extends/surfaces/PaperWrapper'
 import Typography from '../../components/extends/displays/TypographyWrapper'
 import Card from '@mui/material/Card'
 import CardContent from '@mui/material/CardContent'
-import CardMedia from '@mui/material/CardMedia'
 import {CardActionArea} from '@mui/material'
+import BoxWrapper from '../../components/wrap/layouts/BoxWrapper'
 
 const DocumentsPageViewer: React.FC<any> = () => {
   const label = 'Documents Viewer'
 
   return (
-    <Paper elevation={3}>
-      <Typography color='inherit' variant='h5' component='h1'>
-        {label}
+    <Paper elevation={3} sx={{
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      height: '100%', // Ajuste a altura conforme necessário
+      padding: '20px' // Adiciona um pouco de padding para evitar que o Card toque as bordas do Paper
+    }}>
+        <BoxWrapper>
+      <Typography color="inherit" variant="h5" component="h1" sx={{ width: '100%', textAlign: 'center' }}>
       </Typography>
-      <Card sx={{maxWidth: 700}}>
+        </BoxWrapper>        
+      <Card sx={{ maxWidth: 700, margin: 'auto' }}> {/* Adiciona margin auto para centralizar horizontalmente */}
         <CardActionArea>
           <CardContent>
-            <Typography gutterBottom variant='h5' component='div'>
-              Lizard
+            <Typography gutterBottom variant="h4" component="div">
+            {label}
             </Typography>
-            <Typography variant='body2' color='text.secondary'>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+            <Typography variant="body2" color="text.secondary">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
               incididunt ut labore et dolore magna aliqua. Rhoncus dolor purus non enim praesent
               elementum facilisis leo vel. Risus at ultrices mi tempus imperdiet. Semper risus in
               hendrerit gravida rutrum quisque non tellus. Convallis convallis tellus id interdum
@@ -32,11 +39,28 @@ const DocumentsPageViewer: React.FC<any> = () => {
               at augue. At augue eget arcu dictum varius duis at consectetur lorem. Velit sed
               ullamcorper morbi tincidunt. Lorem donec massa sapien faucibus et molestie ac.
             </Typography>
+            <br />
+            <Typography variant="h5" sx={{ mb: 1.5 }} color="text.secondary">
+                Second section
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+              incididunt ut labore et dolore magna aliqua. Rhoncus dolor purus non enim praesent
+              elementum facilisis leo vel. Risus at ultrices mi tempus imperdiet. Semper risus in
+              hendrerit gravida rutrum quisque non tellus. Convallis convallis tellus id interdum
+              velit laoreet id donec ultrices. Odio morbi quis commodo odio aenean sed adipiscing.
+              Amet nisl suscipit adipiscing bibendum est ultricies integer quis. Cursus euismod quis
+              viverra nibh cras. Metus vulputate eu scelerisque felis imperdiet proin fermentum leo.
+              Mauris commodo quis imperdiet massa tincidunt. Cras tincidunt lobortis feugiat vivamus
+              at augue. At augue eget arcu dictum varius duis at consectetur lorem. Velit sed
+              ullamcorper morbi tincidunt. Lorem donec massa sapien faucibus et molestie ac.
+            </Typography>
+            <br />
           </CardContent>
         </CardActionArea>
       </Card>
     </Paper>
-  )
-}
+  );
+};
 
 export default DocumentsPageViewer
