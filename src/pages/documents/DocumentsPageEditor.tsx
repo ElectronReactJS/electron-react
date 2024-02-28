4c163672cb5514c53d9d5ba7a5ef5d788b4ad9eb
Malnati <ricardomalnati@gmail.com>
2024-02-28 01:16:37 -0300

    path: auto commit after npm start

--- src/pages/documents/DocumentsPageEditor.tsx
+++ src/pages/documents/DocumentsPageEditor.tsx
@@ -1,17 +1,44 @@
 // src/pages/documents/DocumentsPageEditor.tsx
-import Paper from '../../components/extends/surfaces/PaperWrapper'
-import Typography from '../../components/extends/displays/TypographyWrapper'
+import React from 'react';
+import Paper from '../../components/extends/surfaces/PaperWrapper';
+import Typography from '../../components/extends/displays/TypographyWrapper';
+import MonacoEditor from 'react-monaco-editor';
 
 const DocumentsPageEditor: React.FC<any> = () => {
-  const label = 'Documents Editor'
+  const label = 'Documents Editor';
 
+  // Função para manipular mudanças no editor
+  const onChange = (newValue: string, e: any) => {
+    console.log('onChange', newValue, e);
+  };
+
+  // Opções de configuração para o Monaco Editor
+  const editorOptions = {
+    selectOnLineNumbers: true,
+    roundedSelection: false,
+    readOnly: false,
+    cursorStyle: 'line' as const, // Use 'as const' para garantir que o tipo seja reconhecido corretamente
+    automaticLayout: true,
+  };
+  
   return (
     <Paper elevation={3}>
-      <Typography color='inherit' variant='h5' component='h1'>
+      <Typography color="inherit" variant="h5" component="h1">
         {label}
       </Typography>
+      <div style={{ height: '500px' }}> {/* Defina o estilo conforme necessário */}
+        <MonacoEditor
+          width="100%"
+          height="400" // Ajuste a altura conforme necessário
+          language="javascript" // Defina a linguagem de programação
+          theme="vs-light" // Tema do editor
+          value={'// Escreva seu código aqui\n'}
+          options={editorOptions}
+          onChange={onChange}
+        />
+      </div>
     </Paper>
-  )
-}
+  );
+};
 
-export default DocumentsPageEditor
+export default DocumentsPageEditor;
