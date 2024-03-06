2c98ab6b1d96c7465c9540290ee385b6e60370d6
Malnati <ricardomalnati@gmail.com>
2024-03-06 15:42:16 -0300

    path: auto commit after npm start

--- src/pages/ai/document/AI.DocumentChat.Page.tsx
+++ src/pages/ai/document/AI.DocumentChat.Page.tsx
@@ -2,44 +2,45 @@
 import React, {useState} from 'react'
 import Page from '../../../components/wrap/layouts/Page'
 import AIVerticalStepper from '../AIVerticalStepper'
-import {Box, Button, Avatar, Paper, BottomNavigation, BottomNavigationAction} from '@mui/material'
+import {Box, Avatar, Paper, BottomNavigation, BottomNavigationAction} from '@mui/material'
 import {AIVerticalStepperType} from '../AIVerticalStepperType'
 
 import RobotIcon from '@mui/icons-material/Android'
 import UserIcon from '@mui/icons-material/Person'
 import ArchiveIcon from '@mui/icons-material/Archive'
 
-import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos'
-import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos'
 import ArrowUpward from '@mui/icons-material/ArrowUpward'
 import ArrowDownward from '@mui/icons-material/ArrowDownward'
-import IconTextFields from '../../../components/ext/displays/IconTextFieldsExt'
 import TextField from '../../../components/wrap/inputs/TextField'
 
 const AIDocumentChatPage: React.FC = () => {
   const [activeStep, setActiveStep] = useState(0)
-  const [steps, setSteps] = useState<AIVerticalStepperType[]>([])
+  const [steps, setSteps] = useState<AIVerticalStepperType[]>([{
+    subject: 'Init',
+    description: 'Hello, how can I help you, now?',
+    avatar: (
+      <Avatar>
+        <RobotIcon />
+      </Avatar>
+    )
+  }])
 
   const handleInputChange = (index: number, field: 'subject' | 'description', value: string) => {
     const newSteps = [...steps]
-    if (!newSteps[index]) {
-      newSteps[index] = {subject: '', description: ''} // Inicializa se necessário
-    }
     newSteps[index][field] = value
     setSteps(newSteps)
   }
 
   const handleNext = () => setActiveStep(prev => prev + 1)
   const handleBack = () => setActiveStep(prev => prev - 1)
-  const handleReset = () => setActiveStep(0)
 
   const addAIStep = () => {
     const newStep: AIVerticalStepperType = {
       subjectInput: (
         <TextField
           required
-          label='Title'
-          placeholder='Add the title here.'
+          label="Title"
+          placeholder="Add the title here."
           onChange={e => handleInputChange(steps.length, 'subject', e.target.value)}
           value={steps[steps.length]?.subject || ''}
         />
@@ -47,8 +48,8 @@ const AIDocumentChatPage: React.FC = () => {
       descriptionInput: (
         <TextField
           required
-          label='Description'
-          placeholder='Add the description here.'
+          label="Description"
+          placeholder="Add the description here."
           onChange={e => handleInputChange(steps.length, 'description', e.target.value)}
           value={steps[steps.length]?.description || ''}
         />
@@ -67,8 +68,8 @@ const AIDocumentChatPage: React.FC = () => {
       subjectInput: (
         <TextField
           required
-          label='Title'
-          placeholder='Add the title here.'
+          label="Title"
+          placeholder="Add the title here."
           onChange={e => handleInputChange(steps.length, 'subject', e.target.value)}
           value={steps[steps.length]?.subject || ''}
         />
@@ -76,8 +77,8 @@ const AIDocumentChatPage: React.FC = () => {
       descriptionInput: (
         <TextField
           required
-          label='Description'
-          placeholder='Add the description here.'
+          label="Description"
+          placeholder="Add the description here."
           onChange={e => handleInputChange(steps.length, 'description', e.target.value)}
           value={steps[steps.length]?.description || ''}
         />
@@ -94,8 +95,8 @@ const AIDocumentChatPage: React.FC = () => {
   const renderStepActions = (stepIndex: number) => (
     <Box sx={{display: 'flex', flexDirection: 'row', pt: 2}}>
       <Box sx={{flex: '1 1 auto'}} />
-      {stepIndex === 0 && <ArrowUpward onClick={handleBack} />}
-      <ArrowDownward onClick={stepIndex === steps.length - 1 ? handleReset : handleNext} />
+      {stepIndex > 0 && <ArrowUpward onClick={handleBack} />}
+      {stepIndex < steps.length - 1 && <ArrowDownward onClick={handleNext} />}
     </Box>
   )
 
@@ -108,9 +109,9 @@ const AIDocumentChatPage: React.FC = () => {
       />
       <Paper sx={{position: 'fixed', bottom: 0, left: 0, right: 0}} elevation={3}>
         <BottomNavigation showLabels>
-          <BottomNavigationAction label='AI' icon={<RobotIcon />} onClick={addAIStep} />
-          <BottomNavigationAction label='User' icon={<UserIcon />} onClick={addUserStep} />
-          <BottomNavigationAction label='Finish' icon={<ArchiveIcon />} />
+          <BottomNavigationAction label="AI" icon={<RobotIcon />} onClick={addAIStep} />
+          <BottomNavigationAction label="User" icon={<UserIcon />} onClick={addUserStep} />
+          <BottomNavigationAction label="Finish" icon={<ArchiveIcon />} />
         </BottomNavigation>
       </Paper>
     </Page>
